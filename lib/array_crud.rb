efe99d6ca77de90d53aae540ba7d4b55c73e674c
ali aziz <aa_connect@outlook.com>
2018-07-14 15:06:39 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   cities = ["Copenhagen", "Berlin", "Venice", "Zurich"]
-cities.push("London")
+cities.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
