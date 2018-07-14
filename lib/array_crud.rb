f57ab66b2de2669a771fc61a08c11566f965ea3e
ali aziz <aa_connect@outlook.com>
2018-07-14 15:13:23 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ end
 def remove_element_from_start_of_array(array)
   cities = ["Copenhagen", "Berlin", "Venice", "Zurich"]
 cities.unshift("wow")
-cities[0]
+cities.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -36,7 +36,9 @@ retrieve[0]
 end
 
 def retrieve_first_element_from_array(array)
-
+  cities = ["Copenhagen", "Berlin", "Venice", "Zurich"]
+  cities.unshift("wow")
+  cities[0]
 end
 
 def retrieve_last_element_from_array(array)
